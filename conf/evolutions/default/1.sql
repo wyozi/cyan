e2de293bd947e7eadf0124f6b78694e23c4a9087
wyozi <joonasmertanen@gmail.com>
2016-02-10 23:15:22 +0200

    Force lowercase in evolutions as well

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,64 +1,64 @@
 # Initial
 
 # --- !Ups
-CREATE TABLE responses (
-  id       SERIAL UNIQUE,
+CREATE TABLE "responses" (
+  "id"       SERIAL UNIQUE,
 
-  name     VARCHAR(64) NOT NULL,
-  response TEXT        NOT NULL,
+  "name"     VARCHAR(64) NOT NULL,
+  "response" TEXT        NOT NULL,
 
-  PRIMARY KEY (id)
+  PRIMARY KEY ("id")
 );
 
-CREATE TABLE products (
-  id         SERIAL UNIQUE,
-  short_name VARCHAR(16) UNIQUE,
+CREATE TABLE "products" (
+  "id"         SERIAL UNIQUE,
+  "short_name" VARCHAR(16) UNIQUE,
 
-  name       VARCHAR(255),
+  "name"       VARCHAR(255),
 
-  PRIMARY KEY (id)
+  PRIMARY KEY ("id")
 );
 
-CREATE TABLE pings (
-  id          SERIAL UNIQUE,
+CREATE TABLE "pings" (
+  "id"          SERIAL UNIQUE,
 
-  product     VARCHAR(255) NOT NULL REFERENCES products (short_name),
-  license     VARCHAR(255) NOT NULL,
-  user_name   VARCHAR(64)  NOT NULL,
-  date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "product"     VARCHAR(255) NOT NULL REFERENCES "products" ("short_name"),
+  "license"     VARCHAR(255) NOT NULL,
+  "user_name"   VARCHAR(64)  NOT NULL,
+  "date"        TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
 
-  response_id INT       DEFAULT NULL REFERENCES responses (id),
-  ip VARCHAR(16) DEFAULT '0.0.0.0',
+  "response_id" INT         DEFAULT NULL REFERENCES "responses" ("id"),
+  "ip"          VARCHAR(16) DEFAULT '0.0.0.0',
 
-  PRIMARY KEY (id)
+  PRIMARY KEY ("id")
 );
 
-CREATE TABLE pingresponses (
-  id          SERIAL UNIQUE,
+CREATE TABLE "pingresponses" (
+  "id"          SERIAL UNIQUE,
 
-  product_id  INT,
-  license     VARCHAR(255),
-  user_name   VARCHAR(64),
+  "product_id"  INT,
+  "license"     VARCHAR(255),
+  "user_name"   VARCHAR(64),
 
-  response_id INT REFERENCES responses (id),
+  "response_id" INT REFERENCES "responses" ("id"),
 
-  PRIMARY KEY (id)
+  PRIMARY KEY ("id")
 );
-CREATE UNIQUE INDEX ping_userlicprod ON pingresponses (product_id, license, user_name);
+CREATE UNIQUE INDEX ping_userlicprod ON "pingresponses" ("product_id", "license", "user_name");
 
-CREATE TABLE pingextras (
-  ping_id INT REFERENCES pings(id) ON DELETE CASCADE,
-  key     VARCHAR(16) NOT NULL,
-  value   TEXT NOT NULL,
+CREATE TABLE "pingextras" (
+  "ping_id" INT REFERENCES "pings" ("id") ON DELETE CASCADE,
+  "key"     VARCHAR(16) NOT NULL,
+  "value"   TEXT        NOT NULL,
 
-  PRIMARY KEY(ping_id, key)
+  PRIMARY KEY ("ping_id", "key")
 );
-CREATE INDEX pingextra_id ON pingextras(ping_id);
+CREATE INDEX pingextra_id ON "pingextras" ("ping_id");
 
 # --- !Downs
 
-DROP TABLE products;
-DROP TABLE pings;
-DROP TABLE responses;
-DROP TABLE pingresponses;
-DROP TABLE pingextras;
+DROP TABLE "products";
+DROP TABLE "pings";
+DROP TABLE "responses";
+DROP TABLE "pingresponses";
+DROP TABLE "pingextras";
