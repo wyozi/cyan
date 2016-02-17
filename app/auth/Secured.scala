a96759ab4faf74e5146576d12e6c3b8e3084b80b
wyozi <joonasmertanen@gmail.com>
2016-02-17 18:18:23 +0200

    Specify admin password using configuration

--- app/auth/Secured.scala
+++ app/auth/Secured.scala
@@ -1,5 +1,6 @@
 package auth
 
+import play.api.Play
 import play.api.mvc._
 
 import scala.concurrent.Future
@@ -9,10 +10,18 @@ import scala.concurrent.Future
   */
 trait Secured { self: Results =>
 
+
   def isLoggedIn(req: Request[_]): Boolean = req.session.get("loggedIn").isDefined
 
   def setLoggedIn(req: Request[_]): Unit = {
     req.session + ("loggedIn", "true")
+  }
+
+  lazy val password = Play.current.configuration.getString("cyan.password")
+
+  private def testAuth(testUser: String, testPass: String): Boolean = (Some("admin"), password) match {
+    case (Some(user), Some(pass)) if user == testUser && pass == testPass => true
+    case _ => false
   }
 
   object SecureAction extends ActionBuilder[Request] {
@@ -23,7 +32,7 @@ trait Secured { self: Results =>
         req.headers.get("Authorization").flatMap { authorization =>
           authorization.split(" ").drop(1).headOption.filter { encoded =>
             new String(org.apache.commons.codec.binary.Base64.decodeBase64(encoded.getBytes)).split(":").toList match {
-              case u :: p :: Nil if u == "admin" && "admin" == p => true
+              case u :: p :: Nil if testAuth(u, p) => true
               case _ => false
             }
           }
