5c3aec886f530548fab29f61dfc8fce761e2a248
wyozi <joonasmertanen@gmail.com>
2016-02-20 18:57:51 +0200

    Make pingtable pop-over use js routes & make it global

--- app/controllers/admin/Main.scala
+++ app/controllers/admin/Main.scala
@@ -3,10 +3,19 @@ package controllers.admin
 import auth.Secured
 import com.google.inject.Inject
 import dao.{ResponsesDAO, PingExtrasDAO, ProductsDAO, PingsDAO}
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
+import play.api.routing.JavaScriptReverseRouter
 
 class Main @Inject() (implicit productsDAO: ProductsDAO, pingsDAO: PingsDAO, pingExtrasDAO: PingExtrasDAO, responsesDAO: ResponsesDAO) extends Controller with Secured {
   def index = SecureAction {
     Ok(views.html.admin.main())
+  }
+
+  def javascriptRoutes = SecureAction { implicit request =>
+    Ok(
+      JavaScriptReverseRouter("jsAdminRoutes")(
+        controllers.admin.routes.javascript.Pings.showPingExtra
+      )
+    ).as("text/javascript")
   }
 }
