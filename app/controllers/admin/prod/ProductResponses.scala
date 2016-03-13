34735c0395fc282aad2077c1f478b8bd051bd8c7
wyozi <joonasmertanen@gmail.com>
2016-03-13 16:24:18 +0200

    Add response insertion panel to prod response view

--- app/controllers/admin/prod/ProductResponses.scala
+++ app/controllers/admin/prod/ProductResponses.scala
@@ -3,9 +3,10 @@ package controllers.admin.prod
 import auth.Secured
 import com.google.inject.Inject
 import dao._
+import play.api.data.Form
 import play.api.mvc.Controller
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class ProductResponses @Inject() ()
   (implicit ec: ExecutionContext,
@@ -22,6 +23,32 @@ class ProductResponses @Inject() ()
       case Some(prod) =>
         Ok(views.html.admin.prod_resp_list(prod))
     }
+  }
+
+  import play.api.data.Forms._
+  val responseForm = Form(
+    tuple(
+      "license" -> text,
+      "user" -> text,
+      "response" -> optional(number)
+    )
+  )
+
+  def insert(prodId: Int) = SecureAction.async { implicit request =>
+    responseForm.bindFromRequest().fold(
+      formWithErrors => Future.successful(BadRequest("Error: " + formWithErrors.toString)), // TODO nicer error display
+      pingResponse => {
+        val licenseOpt = pingResponse._1 match { case "" => None; case x => Some(x) }
+        val userOpt = pingResponse._2 match { case "" => None; case x => Some(x) }
+        val respOpt = pingResponse._3
+
+        pingResponsesDAO
+          .upsertExactPingResponse(Some(prodId), licenseOpt, userOpt, respOpt)
+          .map { x =>
+            Redirect(routes.ProductResponses.list(prodId))
+          }
+      }
+    )
   }
 }
 
