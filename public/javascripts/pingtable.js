132b8c3f430336f88f988031a4851fabfcbb6b3d
wyozi <joonasmertanen@gmail.com>
2017-08-26 15:00:23 +0300

    Make pingtable replace format generic

--- public/javascripts/pingtable.js
+++ public/javascripts/pingtable.js
@@ -26,6 +26,7 @@ $(document).ready(function() {
 
     $('a[data-backend-query-replace]').each(function() {
         var query = $(this).data("backend-query-replace");
+        var replaceFormat = $(this).data("backend-query-replace-format");
         var queryRoute = jsAdminRoutes.controllers.admin.BackendController.view(query, null, null);
 
         var params = ($(this).data("backend-query-params") || "");
@@ -37,7 +38,16 @@ $(document).ready(function() {
             success: function(response) {
                 var data = JSON.parse(response);
                 that.attr("title", that.text());
-                that.text(data.hostname + " (" + data.playerCount + "/" + data.maxPlayerCount + ")");
+
+                var newContent = (replaceFormat || "[no format provided]").replace(/{([^}]*)}/g, function(match, tag) {
+                    if (tag in data) {
+                        return data[tag];
+                    } else {
+                        return '[{' + tag + '} not provided]';
+                    }
+                });
+                that.text(newContent);
+                that.html(that.html().replace(/\n/g,"<br>")); // line breaks to <br>
             }
         });
     });
