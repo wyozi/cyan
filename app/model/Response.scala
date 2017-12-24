1e2e8484181368880d726936141f1b0d14a735fb
Joonas Mertanen <joonas.mertanen@gmail.com>
2017-12-24 18:07:44 +0200

    Show responses on response list as colored buttons

--- app/model/Response.scala
+++ app/model/Response.scala
@@ -3,4 +3,10 @@ package model
 /**
   * Created by wyozi on 3.2.2016.
   */
-case class Response(id: Int, name: String, body: String, color: NotificationColor.NotificationColor)
+case class Response(id: Int, name: String, body: String, color: NotificationColor.NotificationColor) {
+  def bootstrap3ClassSuffix: String = color match {
+    // TODO come up with colors for these
+    case NotificationColor.light | NotificationColor.dark | NotificationColor.secondary => "default"
+    case x => x.toString
+  }
+}
